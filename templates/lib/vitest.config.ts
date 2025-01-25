2017eb4e598fc73ef9e8538134894a0721651eaf
Valentin Richard <valentin.richard@infomaniak.com>
2025-01-25 18:14:58 +0100

    v0.2.7

--- templates/lib/vitest.config.ts
+++ templates/lib/vitest.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    // ...
+    dir: 'src',
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/types/**/*.ts'],
+      thresholds: {
+        100: true,
+      },
+    },
   },
 });
