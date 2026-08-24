a4fed7c11dba971e241fe1ac9247a3cf4d1e82c7
Valentin Richard <valentin.richard@infomaniak.com>
2026-08-24 14:40:13 +0200

    chore: v2.0.13 - update packages

--- src/helpers.private/path/to-absolute-path.ts
+++ src/helpers.private/path/to-absolute-path.ts
@@ -1,4 +1,5 @@
 import { isAbsolute, resolve } from 'node:path';
+import process from 'node:process';
 
 export function toAbsolutePath(path: string, cwd: string = process.cwd()): string {
   return isAbsolute(path) ? path : resolve(cwd, path);
